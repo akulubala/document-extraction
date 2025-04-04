6de872f59651f5e3c392a6c5da7c52cbeef5f9d7
Raymond <akulubala@gmail.com>
2025-04-04 15:20:37 +0800

    fix error

--- document_extraction/utils.py
+++ document_extraction/utils.py
@@ -5,8 +5,8 @@ def loadEnv():
     BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     if os.path.exists(os.path.join(BASE_DIR, '.env')):
         environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
-    elif os.path.exists('/run/secrets/booking_v2_env'):
-        environ.Env.read_env('/run/secrets/booking_v2_env')
+    elif os.path.exists('/run/secrets/booking_v2_ai_env'):
+        environ.Env.read_env('/run/secrets/booking_v2_ai_env')
     else:
         raise Exception("No .env file or secrets file found.")
     return env
